40817ebed873a93799e1505ef19f272ce503ac97
天涯人在 <1065249307@qq.com>
2018-11-16 10:46:30 +0800

    sujiyu

--- src/main/java/com/sysh/mapper/AB10AB01Mapper.java
+++ src/main/java/com/sysh/mapper/AB10AB01Mapper.java
@@ -2,7 +2,6 @@ package com.sysh.mapper;
 
 import com.sysh.dto.AB10_AB01Dto;
 import com.sysh.dto.DropOfSchoolDto;
-import com.sysh.dto.VillageDto;
 
 import java.util.List;
 
@@ -15,14 +14,35 @@ import java.util.List;
  * @since JDK 1.8
  */
 public interface AB10AB01Mapper {
-
+    /**
+     * daw
+     * @param num 帮扶干部编号num
+     * @return long
+     */
     AB10_AB01Dto findFamilyDetailInfo(Long num);
 
+    /**
+     * ada
+     * @param num 帮扶干部编号
+     * @return long
+     */
     List<DropOfSchoolDto> findFamilyStudent(Long num);
     //查询6-16岁在校生情况不为空的贫困户家庭成员有多少
+
+    /**
+     * wada
+     * @param povertyNumber povertyNumber
+     * @return long
+     */
     Long findIsStudent(String povertyNumber);
 
     //查询在校生情况得身份证号
+
+    /**
+     * da
+     * @param povertyNumber PovertyNumber
+     * @return long
+     */
     List<String> findStudentIdNumber(String povertyNumber);
 
 
